fc465b4715db2daee3a5802e24fbd5a24f7b1652
Nicholas Mann <nicholasmann@nicholass-mbp.datto.lan>
2016-05-19 14:44:35 -0400

    Initial Push

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('svg:nth-child(1)').css({
+		marginLeft: '5px',
+		marginTop: '0'
+	});
+	$('svg:nth-child(2)').css({
+		marginLeft: '0',
+		marginTop: '0'
+	});
+	$('svg:nth-child(3)').css({
+		marginLeft: '0',
+	});
+});
